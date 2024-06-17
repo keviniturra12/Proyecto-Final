c46a6aa8bc9418ff8d4ce532b8288255d2114be5
PaulCarrillo96 <paulfranc96@gmail.com>
2024-06-17 18:02:56 -0400

    Cambios para funcionamiento de static
    Se vincularon los .css .js y .html con la configuracion correcta. Ademas se arreglo el error de la vista de los modal.

--- paginaweb/abogados/urls.py
+++ paginaweb/abogados/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('inicio',views.inicio, name='inicio'),
-path('Formulario',views.Formulario, name='Formulario'),
+path('Inicio',views.Inicio, name='Inicio'),
+path('Registro',views.Registro, name='Registro'),
 path('Sobre_nosotros',views.Sobre_nosotros, name='Sobre_nosotros'),
-
-
-
+path('TecnicoJuridico',views.TecnicoJuridico, name='TecnicoJuridico'),
+path('VistaAbogadoAdministrador',views.VistaAbogadoAdministrador, name='VistaAbogadoAdministrador'),
+path('VistaAbogadoConsultor',views.VistaAbogadoConsultor, name='VistaAbogadoConsultor'),
+path('VistaCliente',views.VistaCliente, name='VistaCliente'),
 ]
--- paginaweb/abogados/views.py
+++ paginaweb/abogados/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 
-def inicio(request):
+def Inicio(request):
     context={}
-    return render(request,'abogados/inicio.html', context)
+    return render(request,'abogados/Inicio.html', context)
 
-def Formulario(request):
+def Registro(request):
     context={}
-    return render(request,'abogados/Formulario.html', context)
+    return render(request,'abogados/Registro.html', context)
 
 def Sobre_nosotros(request):
     context={}
     return render(request,'abogados/Sobre_nosotros.html', context)
+
+def TecnicoJuridico(request):
+    context={}
+    return render(request,'abogados/TecnicoJuridico.html', context)
+
+def VistaAbogadoAdministrador(request):
+    context={}
+    return render(request,'abogados/VistaAbogadoAdministrador.html', context)
+
+def VistaAbogadoConsultor(request):
+    context={}
+    return render(request,'abogados/VistaAbogadoConsultor.html', context)
+
+def VistaCliente(request):
+    context={}
+    return render(request,'abogados/VistaCliente.html', context)
 
